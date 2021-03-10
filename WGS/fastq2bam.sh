7d09bf46b35144dda19fb344f14cebe3fca4a784
Developer MJ <67282138+Ming-kim@users.noreply.github.com>
2021-03-10 19:07:12 +0900

    Update fastq2bam.sh

--- WGS/fastq2bam.sh
+++ WGS/fastq2bam.sh
@@ -78,8 +78,8 @@ R2=${outdir}/${sample_id}/FASTQC/${sample_id}.R2.bbduk.r.Q10.fastq.gz
 
 
 echo "[INFO] create bam"
-echo "[INFO] command : bwa mem -R \"@RG\\tID:${sampleID}\\tSM:${sampleID}\\tPL:illumina\" -M -t ${threads} ${ref_hg} {R1} {R2} | samtools view -@ ${threads} -Suh -F 12 -f 2 -q 20 - -o ${outdir}/${sample_id}/BAM/${sample_id}.bam"
-bwa mem -R "@RG\\tID:${sampleID}\\tSM:${sampleID}\\tPL:illumina" -M -t ${threads} ${ref_hg} {R1} {R2} | samtools view -@ ${threads} -Suh -F 12 -f 2 -q 20 - -o ${outdir}/${sample_id}/BAM/${sample_id}.bam
+echo "[INFO] command : bwa mem -R \"@RG\\tID:${sample_id}\\tSM:${sample_id}\\tPL:illumina\" -M -t ${threads} ${ref_hg} ${R1} ${R2} | samtools view -@ ${threads} -Suh -F 12 -f 2 -q 20 - -o ${outdir}/${sample_id}/BAM/${sample_id}.bam"
+bwa mem -R "@RG\\tID:${sample_id}\\tSM:${sample_id}\\tPL:illumina" -M -t ${threads} ${ref_hg} ${R1} ${R2} | samtools view -@ ${threads} -Suh -F 12 -f 2 -q 20 - -o ${outdir}/${sample_id}/BAM/${sample_id}.bam
 
 
 
@@ -90,17 +90,7 @@ samtools sort -@ ${threads} ${outdir}/${sample_id}/BAM/${sample_id}.bam -o ${out
 
 
 echo "[INFO] deduplication"
-echo '''[INFO] command :
-java8 -Xmx${mem_gb}g \
-      -jar /opt/data/picard-2.20.1/picard.jar \
-      MarkDuplicates \
-      REMOVE_SEQUENCING_DUPLICATES=true \
-      INPUT=${outdir}/${sample_id}/BAM/${smaple_id}.sorted.bam \
-      OUTPUT=${threads} ${outdir}/${sample_id}/BAM/${sample_id}.dedup.sorted.bam \
-      METRICS_FILE=${threads} ${outdir}/${sample_id}/BAM/${sample_id}.dedup.sorted.bam.metrics \
-      ASSUME_SORTED=true \
-      VALIDATION_STRINGENCY=SILENT
-'''
+echo "[INFO] command : java8 -Xmx${mem_gb}g -jar /opt/data/picard-2.20.1/picard.jar MarkDuplicates REMOVE_SEQUENCING_DUPLICATES=true INPUT=${outdir}/${sample_id}/BAM/${smaple_id}.sorted.bam OUTPUT=${threads} ${outdir}/${sample_id}/BAM/${sample_id}.dedup.sorted.bam METRICS_FILE=${threads} ${outdir}/${sample_id}/BAM/${sample_id}.dedup.sorted.bam.metrics ASSUME_SORTED=true VALIDATION_STRINGENCY=SILENT"
 java8 -Xmx${mem_gb}g \
       -jar /opt/data/picard-2.20.1/picard.jar \
       MarkDuplicates \
