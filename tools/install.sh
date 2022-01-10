023ae1607209eef4c594160bdb014380243bfdbb
Developer MJ <67282138+minjae-dev@users.noreply.github.com>
2022-01-10 16:59:34 +0900

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -1,24 +1,52 @@
+#!/bin/bash
 # CentOS 7.6.1810
 
-while getopts -l samtools flag
-do
-    case "${flag}" in
-        samtools) 
+usage() {
+    err_msg \
+"Usage: $(basename "$0")
+Short options:          Description
+        -s              Samtools install flag.
+        -o              Install or download path.
+"
+}
+
+err_msg() { echo "$@";} >&2
+
+# check option
+[ $# -eq 0 ] && echo -e \
+"None of option is inserted
+Try 'bash ./install.sh --help' for more information." && exit 0
+
+
+# Get option
+while getopts "o:sh" opt; do
+    case $opt in
+        s)
+            samtools="install"
             ;;
-        \?)	# 지정이외 옵션은 이 문자로 할당된다.
-            echo $@ is not valid option
+        o)
+            outdir=$OPTARG
+            ;;
+        h)
+            usage
+            ;;
+        \?)
+            err_msg "$@ is not valid option"
             exit 0
-            ;;  
+            ;;
     esac
 done
 
-yum install -y wget bzip2 gcc-c++ make ncurses-devel zlib-devel bzip2-devel xz-devel && \
-mkdir ~/tools && \
-TOOLS=~/tools && \
-wget "https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2" -P $TOOLS --no-check-certificate && \
-tar xjf $TOOLS/samtools-1.9.tar.bz2 -C $TOOLS && \
-cd $TOOLS/samtools-1.9 && \
-bash ./configure && \
-make && \
-make install && \
-rm -rf $TOOLS/samtools-1.9.tar.bz2
+if  [[ $samtools -eq "install"  ]] && [[ -v outdir ]]; then
+    yum install -y wget bzip2 gcc-c++ make ncurses-devel zlib-devel bzip2-devel xz-devel && \
+    mkdir  && \
+    TOOLS=~/tools && \
+    wget "https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2" -P $TOOLS --no-check-certificate && \
+    tar xjf $TOOLS/samtools-1.9.tar.bz2 -C $TOOLS && \
+    cd $TOOLS/samtools-1.9 && \
+    bash ./configure && \
+    make && \
+    make install && \
+    rm -rf $TOOLS/samtools-1.9.tar.bz2
+fi
+
